ffea8da810afa073e7ec86ec656d506094b4d3e0
duncanhaystead <duncan.haystead@berkeley.edu>
2018-06-05 13:49:25 -0700

    Update EUV_reader.py

--- EUV_reader.py
+++ EUV_reader.py
@@ -1,3 +1,4 @@
+
 from netCDF4 import Dataset
 import math
 import numpy
@@ -5,14 +6,14 @@ def czm_generator_euv(filename):
     """Writes a czml file from the unit vectors of the 108 vertical positions with epoch along with lon,lat, alt"""
     euvdata = Dataset(filename,"r" )
     unit_vector_vertical = euvdata.variables["ICON_ANCILLARY_EUV_FOV_UNITVECTORS_ECEF"]
-    vertical_time = vertical_time(unit_vector_vertical)
+    vertical = vertical_time(unit_vector_vertical)
     start_file = '[{"version": "1.0", "id": "document"}, {"label": {"text": "ICON", "pixelOffset": {"cartesian2": [0.0, 16.0]}, "scale": 0.5, "show": true}, "path": {"show": false, "material": {"solidColor": {"color": {"rgba": [255, 0, 255, 125]}}}, "width": 2, "trailTime": 0, "resolution": 120, "leadTime": 0, "trailTime": 10000},  "cylinder" : { "length" : 1000000.0, "topRadius" : 100.0, "bottomRadius" : 500000.0, "material" : { "solidColor" : { "color" : { "rgba" : [0, 255, 0, 128] } } }, "outline" : true, "outlineColor" : { "rgba" : [0, 0, 0, 255] } }, "position": {"interpolationDegree": 5, "referenceFrame": "INTERTIAL", "cartographicDegrees":'
-	end_file = '}, "id": "ICON"}]'
-	file_complete = start_file + str(posistion_list).replace("'",'') + str(orientation_list).replace("'",'') + end_file
-	f = open(filename + '.txt', "w+")
-	f.write(file_complete);
-	f.close();
-	return "file written for " + filename[:-2]
+    end_file = '}, "id": "ICON"}]'
+    file_complete = start_file + str(vertical).replace("'",'') + end_file
+    f = open(filename + '.txt', "w+")
+    f.write(file_complete);
+    f.close();
+    return "file written for " + filename[:-2]
     
     
 
@@ -20,12 +21,11 @@ def czm_generator_euv(filename):
 
 
 def vertical_time(matrix):
-    time_vertical_matrix = list(map(list, list(zip(matrix[1], matrix[2]))))
+    time_vertical_matrix = []
+    for i in range (len(matrix)):
+        time_vertical_matrix += list(map(list, list(zip(matrix[i][1], matrix[i][2]))))
     return time_vertical_matrix
-
-
-    
-                                                                    
+                                             
                 
 
 
