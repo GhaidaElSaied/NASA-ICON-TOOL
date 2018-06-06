6228d569a6a1fe7c4084178be5b08ffde671f8ea
Lily Bhattacharjee <lilybhattacharjeeca@gmail.com>
2018-06-06 14:07:43 -0700

    added import statements

--- calc-funcs.py
+++ calc-funcs.py
@@ -1,3 +1,5 @@
+import math, numpy
+
 def convert_time_format(time):
 	"""Converts time stamps from the netCDF to the form for czml "2018-02-09T00:00:00+00:00"""
 	time_string = time[0:10] + "T" + time[11:19] +"+00:00"
