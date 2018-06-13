25beeb46db3a90b031a47d6469b69c2471646579
Lily Bhattacharjee <lilybhattacharjeeca@gmail.com>
2018-06-13 01:39:45 -0700

    reads euv files --> czml, compatible w euv demo

--- EUV_reader.py
+++ EUV_reader.py
@@ -1,12 +1,9 @@
 from netCDF4 import Dataset
-from calc_funcs import convert_time_format, EUV_to_unit_quaternion
+import numpy as np
+from calc_funcs import EUV_to_unit_quaternion
 
 def czml_generator_euv(filename):
 	euvdata = Dataset(filename,"r" )
-
-	icon_x_hat = euvdata.variables["ICON_ANCILLARY_EUV_SC_XHAT"]
-	icon_y_hat = euvdata.variables["ICON_ANCILLARY_EUV_SC_YHAT"]
-	icon_z_hat = euvdata.variables["ICON_ANCILLARY_EUV_SC_ZHAT"]
 
 	time = euvdata.variables["ICON_ANCILLARY_EUV_TIME_UTC_STRING"]
 
@@ -19,38 +16,39 @@ def czml_generator_euv(filename):
 	zenith = euvdata.variables["ICON_ANCILLARY_EUV_FOV_ZENITH_ANGLE"] # 'longitude'
 
 	position_list = positions(lat, lon, alt, time)
-	orientation_list = orientations(azimuth, zenith)
+	orientation_list = orientations(azimuth, zenith, time)
 	time_str = time_string(time[0], time[len(time) - 1])
 	print(time_str)
 	start_file = """[{
 	"id" : "document",
 	"name" : "EUV",
-	"version" : "1.0",
-	"cylinder" : {
-		"id" : "cone",
-		"name" : "EUV FOV",
-		"length" : 400000.0,
-    	"topRadius" : 0.0,
-    	"bottomRadius" : 200000.0,
-    	"material" : {
-        	"solidColor" : {
-            	"color" : {
-                	"rgba" : [255, 0, 0, 255]
-            	}
-        	}
-    	},
-		"availability" : """ + time_str + """,
-		"position" : {
+	"version" : "1.0"},
+	{
 		"interpolationDegree" : 5,
 		"referenceFrame" : "INERTIAL",
-		"cartographicDegrees" : """
-	middle_file = """,
-	"interpolationAlgorithm": "LAGRANGE"},
+		"id" : "cone",
+		"name" : "EUV FOV",
+		"availability" : """ + time_str + """,
+		"cylinder" : {
+			"length" : 400000.0,
+    		"topRadius" : 0.0,
+    		"bottomRadius" : 200000.0,
+    		"material" : {
+        		"solidColor" : {
+            		"color" : {
+                		"rgba" : [255, 0, 0, 255]
+            		}
+        		}
+    		}
+		},
+		"position" : {
+			"cartographicDegrees" : """
+	middle_file = """, "interpolationAlgorithm": "LAGRANGE"},
 	"orientation": {
-		"interpolationAlgorithm":"LINEAR",
+		"interpolationAlgorithm": "LINEAR",
 		"interpolationDegree": 1,
 		"unitQuaternion": """
-	end_file = """}}}]"""
+	end_file = """}}]"""
 	file_complete = start_file + str(position_list).replace("'",'') + middle_file + str(orientation_list).replace("'",'') + end_file
 	f = open(filename[: -3] + '.txt', "w+")
 	f.write(file_complete)
@@ -58,10 +56,10 @@ def czml_generator_euv(filename):
 	return "file written for " + filename[:-3]
 
 # orientations should be recalculated based on where the EUV is looking
-def orientations(azimuth, zenith):
+def orientations(azimuth, zenith, time):
 	orients = []
-	for pair in map(list, zip(azimuth, zenith)):
-		orients += EUV_to_unit_quaternion(pair[0].data.item(0), pair[1].data.item(0))
+	for pair in map(list, zip(azimuth, zenith, time)):
+		orients += [convert_time_format(pair[2])] + EUV_to_unit_quaternion(np.mean(pair[0].data), np.mean(pair[1].data))
 	return orients
 
 # match all times to azimuth & zenith ranges
@@ -78,6 +76,11 @@ def positions(lat,lon,alt,time):
 
 def time_string(start_time, end_time):
 	"""Converts time stamps from the netCDF to the form for czml "2018-02-09T00:00:00+00:00"""
-	start_time_string = start_time[0:10] + "T" + start_time[11:19] +"+00:00"
-	end_time_string = end_time[0:10] + "T" + end_time[11:19] +"+00:00"
+	start_time_string = start_time[0:10] + "T" + start_time[11:19] + "Z"
+	end_time_string = end_time[0:10] + "T" + end_time[11:19] + "Z"
 	return ('"%s/%s"' % (start_time_string, end_time_string))
+
+def convert_time_format(time):
+	"""Converts time stamps from the netCDF to the form for czml "2018-02-09T00:00:00+00:00"""
+	time_string = time[0:10] + "T" + time[11:19] + "Z"
+	return ('"%s"' % time_string)
