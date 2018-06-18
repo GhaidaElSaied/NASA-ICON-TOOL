553b602ccd581f70636f2583c4952ae495773aec
Lily Bhattacharjee <lilybhattacharjeeca@gmail.com>
2018-06-18 10:11:44 -0700

    orientations haven't been calculated yet
    this code should crash bc orientation_list is not defined yet, we need to calculate unit quaternion for each time

---
+++ mighti_reader.py
@@ -0,0 +1,55 @@
+from netCDF4 import Dataset
+import numpy as np
+import math
+import calc_funcs
+
+def czml_generator_mighti(filename):
+  mightidata = Dataset(filename, "r")
+
+  time = mightidata.variables["ICON_ANCILLARY_MIGHTI_TIME_UTC_STRING"]
+  #determine orientation of spacecraft
+  instra_x_hat = mightidata.variables["ICON_ANCILLARY_MIGHTI_INSTRA_X_ECEF"]
+  instra_y_hat = mightidata.variables["ICON_ANCILLARY_MIGHTI_INSTRA_Y_ECEF"]
+  instra_z_hat = mightidata.variables["ICON_ANCILLARY_MIGHTI_INSTRA_Z_ECEF"]
+  
+  #determine position of spacecraft
+  lat = mightidata.variables["ICON_ANCILLARY_MIGHTI_LATITUDE"]
+  lon = mightidata.variables["ICON_ANCILLARY_MIGHTI_LONGITUDE"]
+  alt = mightidata.variables["ICON_ANCILLARY_MIGHTI_ALTITUDE"]
+
+  position_list = calc_funcs.positions(lat, lon, alt, time)
+  orientation_list = calc_funcs.orientations(instra_x_hat, instra_y_hat, instra_z_hat, time)
+
+  start_file = """[{"version": "1.0", "id": "document"}, 
+  {"cylinder" : {
+    "length" : 1000000.0, 
+    "topRadius" : 100.0, 
+    "bottomRadius" : 500000.0, 
+    "material" : { 
+      "solidColor" : { 
+        "color" : { 
+          "rgba" : [0, 255, 0, 128] 
+        } 
+      } 
+    }, 
+    "outline" : true, 
+    "outlineColor" : { 
+      "rgba" : [0, 0, 0, 255] 
+    } 
+  }, 
+  "position": {
+    "interpolationDegree": 5, 
+    "referenceFrame": "INERTIAL", 
+    "cartographicDegrees":"""
+  middle_file = """, 
+    "interpolationAlgorithm": "LAGRANGE"
+    },
+    "orientation": {
+      "interpolationAlgorithm":"LINEAR", 
+      "interpolationDegree": 1, 
+      "unitQuaternion":"""
+  end_file = '}}]'
+  file_complete = start_file + str(position_list).replace("'", '') + middle_file + str(orientation_list).replace("'",'')+ end_file
+  f.write(file_complete)
+  f.close;
+  return "file written for" + filename
