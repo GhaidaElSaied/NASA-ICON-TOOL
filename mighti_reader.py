6e1c5b0c244314e7ae6779bd13aecc9fa65b60b4
duncanhaystead <duncan.haystead@berkeley.edu>
2018-06-18 10:58:29 -0700

    Updated reader so that it outputs path

--- mighti_reader.py
+++ mighti_reader.py
@@ -20,35 +20,65 @@ def czml_generator_mighti(filename):
   position_list = calc-funcs.positions(lat, lon, alt, time)
   orientation_list = calc-funcs.orientations(instra_x_hat, instra_y_hat, instra_z_hat, time)
 
-  start_file = """[{"version": "1.0", "id": "document"}, 
-  {"cylinder" : {
-    "length" : 1000000.0, 
-    "topRadius" : 100.0, 
-    "bottomRadius" : 500000.0, 
-    "material" : { 
-      "solidColor" : { 
-        "color" : { 
-          "rgba" : [0, 255, 0, 128] 
-        } 
-      } 
-    }, 
-    "outline" : true, 
-    "outlineColor" : { 
-      "rgba" : [0, 0, 0, 255] 
-    } 
-  }, 
-  "position": {
-    "interpolationDegree": 5, 
-    "referenceFrame": "INERTIAL", 
-    "cartographicDegrees":"""
-  middle_file = """, 
-    "interpolationAlgorithm": "LAGRANGE"
-    },
-    "orientation": {
-      "interpolationAlgorithm":"LINEAR", 
-      "interpolationDegree": 1, 
-      "unitQuaternion":"""
-  end_file = '}}]'
+ 	label_start = """[{"version": "1.0", "id": "document"}, {"label":
+		{"text": "ICON",
+		"pixelOffset": {
+			"cartesian2": [0.0, 16.0]},
+			"scale": 0.5,
+			"show": true
+		}, "position" : {
+			"cartographicDegrees" :
+	"""
+	path_start = """[{"version": "1.0", "id": "document"}, {
+		"id" : "orbitPath",
+		"path": {
+				"show" : true,
+				"width": 2,
+				"trailTime": 0,
+				"resolution": 120,
+				"leadTime": 0,
+				"trailTime": 10000,
+				"material": {
+					"solidColor": {
+						"color": {
+							"rgba": [255, 0, 255, 125]
+						}
+					}
+				}
+			},
+				"position": {
+					"cartographicDegrees" :"""
+	start_file = """[{"version": "1.0", "id": "document"},
+		{"interpolationDegree": 5,
+		"referenceFrame": "INERTIAL",
+		"id" : "ivma",
+		"name" : "IVM-""" + type + """FOV\"
+		"cylinder" : {
+			"length" : 1000000.0,
+			"topRadius" : 0.0,
+			"bottomRadius" : 500000.0,
+			"material" : {
+				"solidColor" : {
+					"color" : {
+						"rgba" : [0, 255, 0, 128]
+					}
+				}
+			},
+			"outline" : true,
+			"outlineColor" : {
+				"rgba" : [0, 0, 0, 255]
+			}
+		},
+		"position": {
+			"cartographicDegrees":"""
+	middle_file = """,
+			"interpolationAlgorithm": "LAGRANGE"
+		},
+		"orientation": {
+			"interpolationAlgorithm":"LINEAR",
+			"interpolationDegree":1,
+			"unitQuaternion":"""
+	end_file = '}}]'
   file_complete = start_file + str(position_list).replace("'", '') + middle_file + str(orientation_list).replace("'",'')+ end_file
   f.write(file_complete)
   f.close;
