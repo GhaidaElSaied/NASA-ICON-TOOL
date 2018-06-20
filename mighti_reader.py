baaf756c9a38364d29089ae2dde8d5df164aeb36
duncanhaystead <duncan.haystead@berkeley.edu>
2018-06-20 13:06:26 -0700

    Fixed bugs

--- mighti_reader.py
+++ mighti_reader.py
@@ -1,22 +1,24 @@
 from netCDF4 import Dataset
-import numpy as np
-import math
-import calc_funcs
+import calc_funcs, numpy as np
 
 def czml_generator_mighti(filename):
   mightidata = Dataset(filename, "r")
   type = filename.split("_")[2].split("-")[1]
 
-  time = mightidata.variables["ICON_ANCILLARY_MIGHTI_TIME_UTC_STRING"]
-  #determine orientation of MIGTHI instrument
-  azimuth  = mightidata.variables["ICON_ANCILLARY_MIGHTI_FOV_AZIMUTH"]
-  #determine position of spacecraft
+
+  time = mightidata.variables["ICON_ANCILLARY_MIGHTI_TIME_UTC_STRING"][:, 1]
+
+
+  #read position of spacecraft
   lat = mightidata.variables["ICON_ANCILLARY_MIGHTI_LATITUDE"]
   lon = mightidata.variables["ICON_ANCILLARY_MIGHTI_LONGITUDE"]
   alt = mightidata.variables["ICON_ANCILLARY_MIGHTI_ALTITUDE"]
 
+  #read orientation of MIGHTI instrument
+  azimuth = mightidata.variables["ICON_ANCILLARY_MIGHTI_FOV_AZIMUTH_ANGLE"]
+
   position_list = calc_funcs.positions(lat, lon, alt, time)
-  orientation_list = calc_funcs.orientations(azimuth, 0, time)
+  orientation_list = calc_funcs.orientations_horizontal_coordinate(azimuth, np.zeros(len(azimuth)), time)
   start_file = """[{"version": "1.0", "id": "document"},
 		{"interpolationDegree": 5,
 		"referenceFrame": "INERTIAL",
@@ -49,9 +51,9 @@ def czml_generator_mighti(filename):
 			"unitQuaternion":"""
   end_file = '}}]'
   file_complete = start_file + str(position_list).replace("'", '') + middle_file + str(orientation_list).replace("'",'')+ end_file
-
+  f = open(filename[: -3] + '.txt', "w+")
   f.write(file_complete)
-  f.close()
+  f.close();
 
-   
+
   return "file written for" + filename
