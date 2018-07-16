27334b34bc1221c339ce58c6e4702625dfebcb51
duncanhaystead <duncan.haystead@berkeley.edu>
2018-07-16 10:50:14 -0700

    Add files via upload

--- mighti_reader.py
+++ mighti_reader.py
@@ -18,31 +18,34 @@ def czml_generator_mighti(filename):
   sc_ecef_position = mightidata.variables["ICON_ANCILLARY_MIGHTI_SC_POSITION_ECEF"][:, :, 1]
 
   #read orientation of spacecraft in ECEF
-  x_hat =mightidata.variables["ICON_ANCILLARY_MIGHTI_SC_XHAT"]
-  y_hat = mightidata.variables["ICON_ANCILLARY_MIGHTI_SC_YHAT"]
-  z_hat = mightidata.variables["ICON_ANCILLARY_MIGHTI_SC_ZHAT"]
+  x_hat =mightidata.variables["ICON_ANCILLARY_MIGHTI_SC_XHAT"][:][1]
+  y_hat = mightidata.variables["ICON_ANCILLARY_MIGHTI_SC_YHAT"][:][1]
+  z_hat = mightidata.variables["ICON_ANCILLARY_MIGHTI_SC_ZHAT"][:][1]
   #read orientation of MIGHTI instrument in ECEF
   mighti_FOV = mightidata.variables["ICON_ANCILLARY_MIGHTI_FOV_UNITVECTORS_ECEF"]
   bottom_left =  mighti_FOV[:,0,0,:,1]
   bottom_right = mighti_FOV[:,2,0,:,1]
-  top_left_= mighti_FOV[:,0,2,:,1]
+  top_left= mighti_FOV[:,0,2,:,1]
   top_right= mighti_FOV[:,2,2,:,1]
 
   #position of spacecraft
   position_list = calc_funcs.positions(lat, lon, alt, time)
    #ECEF postion of spacecraft
-  mighti_ecef_pos = calc_funcs.ecef_position_list(sc_ecef_position)
-  #FOV vectors in quaternion
+  ecef_pos = calc_funcs.ecef_position_list(sc_ecef_position)
+
+  #put FOV vectors in quaternion
   bottom_left_quat = calc_funcs.mighti_orientations(bottom_left, bottom_right, top_left, top_right)[0]
   bottom_right_quat = calc_funcs.mighti_orientations(bottom_left, bottom_right, top_left, top_right)[1]
   top_left_quat = calc_funcs.mighti_orientations(bottom_left, bottom_right, top_left, top_right)[2]
   top_right_quat = calc_funcs.mighti_orientations(bottom_left, bottom_right, top_left, top_right)[3]
   #rotate the ECEF positions by the various FOV quaternions and put in unit quaternion form
-  bottom_left_quat_final = calc_funcs.unit_quaternion_mighti_fov(bottom_left_quat, position_list)
-  bottom_right_quat_final = calc_funcs.unit_quaternion_mighti_fov(bottom_right_quat, position_list)
-  top_left_quat_final = calc_funcs.unit_quaternion_mighti_fov(top_left_quat, position_list)
-  top_right_quat_final = calc_funcs.unit_quaternion_mighti_fov(top_right_quat, position_list)
-
+  bottom_left_quat_final = calc_funcs.unit_quaternion_mighti_fov(bottom_left_quat, ecef_pos)
+  bottom_right_quat_final = calc_funcs.unit_quaternion_mighti_fov(bottom_right_quat, ecef_pos)
+  top_left_quat_final = calc_funcs.unit_quaternion_mighti_fov(top_left_quat, ecef_pos)
+  top_right_quat_final = calc_funcs.unit_quaternion_mighti_fov(top_right_quat, ecef_pos)
+  #compute the final orientation FOV
+  quaternion_list = [bottom_left_quat_final, bottom_right_quat_final, top_left_quat_final, top_right_quat_final]
+  orientation_FOV = quaternion_final = calc_funcs.final_mighti_quat(quaternion_list, x_hat, y_hat, z_hat, time)
 
 
 
@@ -51,24 +54,15 @@ def czml_generator_mighti(filename):
 		"referenceFrame": "INERTIAL",
 		"id" : "mighti",
 		"name" : "MIGHTI-""" + type + """FOV\"
-		"cylinder" : {
-			"length" : 1000000.0,
-			"topRadius" : 0.0,
-			"bottomRadius" : 500000.0,
-			"material" : {
-				"solidColor" : {
-					"color" : {
-						"rgba" : [0, 255, 0, 128]
-					}
-				}
-			},
-			"outline" : true,
-			"outlineColor" : {
-				"rgba" : [0, 0, 0, 255]
-			}
+		"model" : {
+			"gltf": "MIGHTI_FOV.gltf",
+            "color" : "black",
+
+            }
 		},
 		"position": {
 			"cartographicDegrees":"""
+
   middle_file = """,
 			"interpolationAlgorithm": "LAGRANGE"
 		},
@@ -77,10 +71,12 @@ def czml_generator_mighti(filename):
 			"interpolationDegree":1,
 			"unitQuaternion":"""
   end_file = '}}]'
-  file_complete = start_file + str(position_list).replace("'", '') + middle_file + str(orientation_list).replace("'",'')+ end_file
-  f = open(filename + '.txt', "w+")
+  file_complete = start_file + str(position_list).replace("'", '') + middle_file + str(orientation_FOV).replace("'",'')+ end_file
+  f = open("mighti_test" + '.txt', "w+")
   f.write(file_complete)
   f.close();
 
 
   return "file written for" + filename
+
+czml_generator_mighti("ICON_L0P_MIGHTI-A_Ancillary_2017-05-27_v01r000.NC")
