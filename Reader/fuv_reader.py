c57a879a2fc2f636985bbbf6cc876f3562e85c05
duncanhaystead <duncan.haystead@berkeley.edu>
2018-08-21 14:22:05 -0700

    FUV reader-JSON may need work

--- Reader/fuv_reader.py
+++ Reader/fuv_reader.py
@@ -8,6 +8,7 @@ def czml_generator_fuv(filename):
     lon = fuvdata.variables["ICON_ANCILLARY_FUV_LONGITUDE"]
     alt = fuvdata.variables["ICON_ANCILLARY_FUV_ALTITUDE"]
     time = fuvdata.variables["ICON_ANCILLARY_FUV_TIME_UTC"]
+    #find azimuth,zenith angles for botton left, bottom right, top right, and top left of instrument
     azimuth_bottom_left = fuvdata.variables["ICON_ANCILLARY_FUV_FOV_AZIMUTH_ANGLE"][:, 0, 0].tolist()
     zenith_bottom_left = fuvdata.variables["ICON_ANCILLARY_FUV_FOV_ZENITH_ANGLE"][:, 0, 0].tolist()
     azimuth_bottom_right = fuvdata.variables["ICON_ANCILLARY_FUV_FOV_AZIMUTH_ANGLE"][:, 0, 5].tolist()
@@ -16,10 +17,12 @@ def czml_generator_fuv(filename):
     zenith_top_right = fuvdata.variables["ICON_ANCILLARY_FUV_FOV_ZENITH_ANGLE"][:, 255, 5].tolist()
     azimuth_top_left = fuvdata.variables["ICON_ANCILLARY_FUV_FOV_AZIMUTH_ANGLE"][:, 255, 0].tolist()
     zenith_top_left = fuvdata.variables["ICON_ANCILLARY_FUV_FOV_ZENITH_ANGLE"][:, 255, 0].tolist()
+    #compute quaternions from azimuth, zenith angles for four positions in FOV
     bottom_left_quat = calc_funcs.fuv_orientation_to_unit_quaternion(azimuth_bottom_left, zenith_bottom_left)
     bottom_right_quat = calc_funcs.fuv_orientation_to_unit_quaternion(azimuth_bottom_right, zenith_bottom_right)
     top_right_quat = calc_funcs.fuv_orientation_to_unit_quaternion(azimuth_top_right, zenith_top_right)
     top_left_quat = calc_funcs.fuv_orientation_to_unit_quaternion(azimuth_top_left, zenith_top_left)
+    #rotate quaternions of FOV positions in counterclockwise direction; creates a list with time string followed by quaternion
     orientations_list = calc_funcs.final_fuv_orientations(bottom_left_quat, bottom_right_quat, top_right_quat, top_left_quat, time)
     positions_list = positions(lat, lon, alt, time)
 
@@ -51,7 +54,3 @@ f = open(filename[: -3] + '.txt', "w+")
 f.write(file_complete)
 f.close();
 return "file written for " + filename[:-3]
-
-
-
-czml_generator_fuv("ICON_L0P_FUV_Ancillary_2017-05-27_v01r000.NC")
