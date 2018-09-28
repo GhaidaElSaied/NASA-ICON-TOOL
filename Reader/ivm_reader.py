68e354491591b82f294770f6874340539aaffd71
duncanhaystead <duncan.haystead@berkeley.edu>
2018-09-28 12:37:37 -0700

    Generic code; checks data; JSON needed

--- Reader/ivm_reader.py
+++ Reader/ivm_reader.py
@@ -6,96 +6,36 @@ import calc_funcs
 def czml_generator_ivm(filename):
 	""" Writes a czml file with the orientation and posistion data for the cone defined by the field of view of the IVM"""
 	fovdata = Dataset(filename,"r")
-	type = filename.split("_")[2].split("-")[1]
-	#extract ivma fov unit vectors
-	ivma_x_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_XHAT_ECEF"][:, ]
-	ivma_y_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_YHAT_ECEF"][:, ]
-	ivma_z_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_ZHAT_ECEF"][:, ]
-	#convert ivma fov unit vectors to ivmb fov unit vectors
-	sc_x_hat = fovdata.variables["ICON_ANCILLARY_IVM_SC_XHAT_ECEF"][:].tolist()
-	ivmb_x_hat, ivmb_y_hat = calc_funcs.rotate_for_ivmb(ivma_x_hat, ivma_y_hat)
-
+	split_filename = filename.split("_")
+	type = split_filename[2].split("-")[1]
 	time = fovdata.variables["ICON_ANCILLARY_IVM_TIME_UTC"]
 	lat = fovdata.variables["ICON_ANCILLARY_IVM_LATITUDE"]
 	lon = fovdata.variables["ICON_ANCILLARY_IVM_LONGITUDE"]
 	alt = fovdata.variables["ICON_ANCILLARY_IVM_ALTITUDE"]
 	position_list = calc_funcs.positions(lat,lon,alt,time)
-	ivma_orientations = calc_funcs.FOV_ivm_orientations(ivma_x_hat, ivma_y_hat, ivma_z_hat, time)
-	ivmb_orientations = calc_funcs.orientations(ivmb_x_hat, ivmb_y_hat, ivma_z_hat, time)
+	if type == "A":
+		ivma_x_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_XHAT_ECEF"][:].tolist()
+		ivma_y_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_YHAT_ECEF"][:].tolist()
+		ivma_z_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_ZHAT_ECEF"][:].tolist()
+		time = time[:].tolist()
+		#check to make sure there is no missing/corrupt data
+		ivma_x_hat, ivma_y_hat, ivma_z_hat, time_orient = calc_funcs.ivm_check_values(ivma_x_hat, ivma_y_hat, ivma_z_hat, time)
+		#convert ivma fov unit vectors to ivmb fov unit vectors
+		ivmb_x_hat, ivmb_y_hat = calc_funcs.rotate_to_other_ivm(ivma_x_hat, ivma_y_hat)
+		ivmb_z_hat = ivma_z_hat
+	else:
+		ivmb_x_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_XHAT_ECEF"][:].tolist()
+		ivmb_y_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_YHAT_ECEF"][:].tolist()
+		ivmb_z_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_ZHAT_ECEF"][:].tolist()
+		#check to make sure there is no missing/corrupt data
+		ivmb_x_hat, ivmb_y_hat, ivmb_z_hat, time_orient = calc_funcs.ivm_check_values(ivma_x_hat, ivma_y_hat, ivma_z_hat, time)
+		#convert ivma fov unit vectors to ivmb fov unit vectors
+		ivma_x_hat, ivma_y_hat = calc_funcs.rotate_to_other_ivm(ivma_x_hat, ivma_y_hat)
+		ivma_z_hat = ivmb_z_hat
 
 
-	label_start = """[{"version": "1.0", "id": "document"}, {"label":
-		{"text": "ICON",
-		"pixelOffset": {
-			"cartesian2": [0.0, 16.0]},
-			"scale": 0.5,
-			"show": true
-		}, "position" : {
-			"cartographicDegrees" :
-	"""
-	path_start = """[{"version": "1.0", "id": "document"}, {
-		"id" : "orbitPath",
-		"path": {
-				"show" : true,
-				"width": 2,
-				"trailTime": 0,
-				"resolution": 120,
-				"leadTime": 0,
-				"trailTime": 10000,
-				"material": {
-					"solidColor": {
-						"color": {
-							"rgba": [255, 0, 255, 125]
-						}
-					}
-				}
-			},
-				"position": {
-					"cartographicDegrees" :"""
-	ivma_file = """[{"version": "1.0", "id": "document"},
-		{"interpolationDegree": 5,
-		"referenceFrame": "INERTIAL",
-		"id" : "ivma",
-		"name" : "IVM-AFOV",
-		"model" : {
-			"show" : true,
-			"gltf" : "cone.gltf",
-			"scale" : 50000000.0,
-			"silhouetteColor" : {
-				"rgba" : [0, 0, 0, 255]
-			},
-			"color": {
-			 	"rgba" : [0, 255, 0, 128]
-			}
-		},
-		"position": {
-			"cartographicDegrees":"""
-	ivmb_file = """[{"version": "1.0", "id": "document"},
-		{"interpolationDegree": 5,
-		"referenceFrame": "INERTIAL",
-		"id" : "ivma",
-		"name" : "IVM-AFOV",
-		"model" : {
-			"show" : true,
-			"gltf" : "cone.gltf",
-			"scale" : "50000000.0",
-			"silhouetteColor" : {
-				"rgba" : [0, 0, 0, 255]
-			},
-			"color": {
-			 	"rgba" : [0, 255, 0, 128]
-			}
-		},
-		"position": {
-		"cartographicDegrees":"""
-	middle_file = """,
-			"interpolationAlgorithm": "LAGRANGE"
-		},
-		"orientation": {
-			"interpolationAlgorithm":"LINEAR",
-			"interpolationDegree":1,
-			"unitQuaternion":"""
-	end_file = '}}]'
+	ivma_orientations = calc_funcs.ivm_orientations_calc(ivma_x_hat, ivma_y_hat, ivma_z_hat, time_orient)
+	ivmb_orientations = calc_funcs.ivm_orientations_calc(ivmb_x_hat, ivmb_y_hat, ivmb_z_hat, time_orient)
 
 
 
@@ -107,27 +47,13 @@ def czml_generator_ivm(filename):
 	label_file = label_start + position_str + end_file
 	path_file = path_start + position_str + end_file
 
-	f_a = open(filename[:-3] + '.txt', "w+")
-	f_b = open(filename[:-3] + 'B' + '.txt', "w+")
-	#label_f = open("label.txt", "w+")
-	#path_f = open("path.txt", "w+")
+	f_a = open(filename + '.txt', "w+")
+	f_b = open(filename + "B" + '.txt', "w+")
 
 	f_a.write(ivma_file_complete);
 	f_b.write(ivmb_file_complete)
-	#label_f.write(label_file)
-	#path_f.write(path_file)
+
 
 	f_a.close()
 	f_b.close()
-	#label_f.close()
-	#path_f.close()
-
-	return "files written for " + filename[:-2]
-#czml_generator_ivm("ICON_L0P_IVM-A_Ancillary_2017-05-27_v01r001.NC")
-#czml_generator_ivm("ICON_L0P_IVM-A_Ancillary_2017-05-28_v01r001.NC")
-#czml_generator_ivm("ICON_L0P_IVM-A_Ancillary_2017-05-29_v01r001.NC")
-
-
-ivma_x_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_XHAT_ECEF"][:, ]
-ivma_y_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_YHAT_ECEF"][:, ]
-ivma_z_hat = fovdata.variables["ICON_ANCILLARY_IVM_INSTRA_ZHAT_ECEF"][:, ]
+	return "file written for"  + filename[:-2]
