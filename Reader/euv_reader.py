a466839339a8a4f7db59f8c44ecefb68a4c549ef
duncanhaystead <duncan.haystead@berkeley.edu>
2018-09-27 18:08:06 -0700

    Updated to check missing data; generic code-JSON may need work

--- Reader/euv_reader.py
+++ Reader/euv_reader.py
@@ -12,12 +12,12 @@ def czml_generator_euv(filename):
 	lon = euvdata.variables["ICON_ANCILLARY_EUV_LONGITUDE"]
 	alt = euvdata.variables["ICON_ANCILLARY_EUV_ALTITUDE"]
 
-	azimuth = euvdata.variables["ICON_ANCILLARY_EUV_FOV_AZIMUTH_ANGLE"] # 'latitude'
-	zenith = euvdata.variables["ICON_ANCILLARY_EUV_FOV_ZENITH_ANGLE"] # 'longitude'
-	#Finds ECI position of EUV
+	azimuth = euvdata.variables["ICON_ANCILLARY_EUV_FOV_AZIMUTH_ANGLE"][:, ].tolist()
+	zenith = euvdata.variables["ICON_ANCILLARY_EUV_FOV_ZENITH_ANGLE"].[:, ]tolist()
+
 	position_list = calc_funcs.positions(lat, lon, alt, time)
-	#Converts 108 measurements azimuth and zenith at each time to an averaged quaternion
-	orientation_list = calc_funcs.euv_orientation_to_unit_quaternion(azimuth, zenith, time)
+	azimuth, zenith, time_orient = calc_funcs.euv_check_values(azimuth, zenith, time[:].tolist())
+	orientation_list = calc_funcs.euv_orientations_calc(azimuth, zenith, time_orient)
 
 	start_file = """[{"version": "1.0", "id": "document"},
   		{"interpolationDegree": 5,
@@ -26,7 +26,7 @@ def czml_generator_euv(filename):
 		"interpolationDegree" : 5,
 		"referenceFrame" : "INERTIAL",
 		"id" : "model",
-		"name" : "UV FOV",,
+		"name" : """ + type + "UV FOV"""",
 		"availability" : """ + time_str + """,
 		"model" : {
 			"show" : true,
@@ -54,3 +54,7 @@ def czml_generator_euv(filename):
 	f.write(file_complete)
 	f.close();
 	return "file written for " + filename[:-3]
+euvdata = Dataset("ICON_L0P_EUV_Ancillary_2017-05-27_v01r001.NC", "r")
+azimuth = euvdata.variables["ICON_ANCILLARY_EUV_FOV_AZIMUTH_ANGLE"]
+zenith = euvdata.variables["ICON_ANCILLARY_EUV_FOV_ZENITH_ANGLE"]
+time = euvdata.variables["ICON_ANCILLARY_EUV_TIME_UTC_STRING"]
