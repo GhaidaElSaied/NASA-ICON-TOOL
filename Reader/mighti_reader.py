bcf074891ce6b7c21fc6df2ad5b028aa555be0e8
duncanhaystead <duncan.haystead@berkeley.edu>
2018-10-09 12:11:52 -0700

    Add files via upload

--- Reader/mighti_reader.py
+++ Reader/mighti_reader.py
@@ -13,43 +13,45 @@ def czml_generator_mighti(filename):
   #read orientation of spacecraft in ECEF
   mighti_fov = mightidata.variables["ICON_ANCILLARY_MIGHTI_FOV_UNITVECTORS_ECEF"]
   #extract vectors for four corners
-  bottom_left_vectors = mighti_fov[:,0, 0, :, 1].tolist()
-  bottom_right_vectors = mighti_fov[:,2, 0, :, 1].tolist()
-  top_right_vectors = mighti_fov[:,2, 2, :, 1].tolist()
-  top_left_vectors = mighti_fov[:, 0, 2, :, 1].tolist()
+  bottom_left_vectors = mighti_fov[:,1, :, 0, 0].tolist()
+  bottom_right_vectors = mighti_fov[:,1, :, 0, 2].tolist()
+  top_right_vectors = mighti_fov[:,1, :, 2, 2].tolist()
+  top_left_vectors = mighti_fov[:,1, :, 2, 0].tolist()
   b_l_vectors, b_r_vectors, t_r_vectors, t_l_vectors, time_orient = calc_funcs.mighti_check_values(bottom_left_vectors, bottom_right_vectors, top_right_vectors, top_left_vectors, time.tolist())
 
   orientation_list = calc_funcs.mighti_orientation_calc(b_l_vectors, b_r_vectors, t_r_vectors, t_l_vectors, time_orient)
 
 
-  start_file = """[{"version": "1.0", "id": "document"},
-		{"interpolationDegree": 5,
-		"referenceFrame": "INERTIAL",
-		"id" : "mighti",
-		"name" : "MIGHTI-""" + type + """FOV\"
-		"model" : {
-			"gltf": "Models/mighti.gltf",
-            "color" : "black",
-            "silhouetteColor": "black",
-            "scale" : "50000000",
-            }
-		},
-		"position": {
-			"cartographicDegrees":"""
+  # start_file = """[{"version": "1.0", "id": "document"},
+	# 	{"interpolationDegree": 5,
+	# 	"referenceFrame": "INERTIAL",
+	# 	"id" : "mighti",
+	# 	"name" : "MIGHTI-""" + type + """FOV\"
+	# 	"model" : {
+	# 		"gltf": "Models/mighti.gltf",
+  #           "color" : "black",
+  #           "silhouetteColor": "black",
+  #           "scale" : "50000000",
+  #           }
+	# 	},
+	# 	"position": {
+	# 		"cartographicDegrees":"""
+  #
+  # middle_file = """,
+	# 		"interpolationAlgorithm": "LAGRANGE"
+	# 	},
+	# 	"orientation": {
+	# 		"interpolationAlgorithm":"LINEAR",
+	# 		"interpolationDegree":1,
+	# 		"unitQuaternion":"""
+  # end_file = '}}]'
+  #
+  # file_complete = start_file + str(position_list).replace("'", '') + middle_file + str(orientation_list).replace("'",'')+ end_file
+  # f = open(filename[:-3] + '.txt', "w+")
+  # f.write(file_complete)
+  # f.close()
 
-  middle_file = """,
-			"interpolationAlgorithm": "LAGRANGE"
-		},
-		"orientation": {
-			"interpolationAlgorithm":"LINEAR",
-			"interpolationDegree":1,
-			"unitQuaternion":"""
-  end_file = '}}]'
 
-  file_complete = start_file + str(position_list).replace("'", '') + middle_file + str(orientation_list).replace("'",'')+ end_file
-  f = open(filename[:-3] + '.txt', "w+")
-  f.write(file_complete)
-  f.close()
+  return orientation_list, lat, lon, alt
 
-
-  return "file written for" + filename
+a, b, c, d = czml_generator_mighti("ICON_L0P_MIGHTI-A_Ancillary_2017-05-27_v01r001.NC")
